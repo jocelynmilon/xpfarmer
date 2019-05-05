b14c42792d398359e3581444ebdbcaf221f7e099
jocelyn Milon <jocelyn.milon@gmail.com>
2019-05-05 19:51:02 +0200

    debut projet

--- src/app/authentification/new/new.component.ts
+++ src/app/authentification/new/new.component.ts
@@ -1,19 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
-import { AuthentificationService } from '../../service/authentification.service';
+import { AuthentificationService } from "../../service/authentification.service";
 
 @Component({
-  selector: 'app-new',
-  templateUrl: './new.component.html',
-  styleUrls: ['./new.component.css']
+  selector: "app-new",
+  templateUrl: "./new.component.html",
+  styleUrls: ["./new.component.css"]
 })
 export class NewComponent implements OnInit {
+  form: FormGroup;
+  users = [];
+  error = "";
 
-  constructor(private authentificationService:AuthentificationService) { }
+  stat = [
+    { id: 1, name: "money", value: 1 },
+    { id: 2, name: "level", value: 1 },
+    { id: 3, name: "levelPoint", value: 1 }
+  ];
+
+  constructor(
+    private authentificationService: AuthentificationService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
-    this.authentificationService.getUsers()
-    .subscribe(data => console.log(data))
+    this.authentificationService.getUsers().subscribe(
+      data => (this.users = data),
+      error => {
+        console.error(error);
+        this.error = error;
+      }
+    );
+    this.form = this.formBuilder.group({
+      firstName: "",
+      lastName: "",
+      avatar: "",
+      stats: ""
+    });
   }
-
+  createUser() {
+    console.log(this.form.value);
+  }
 }
--- src/app/service/authentification.service.ts
+++ src/app/service/authentification.service.ts
@@ -1,28 +1,22 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
-import { Users } from '../models/model.user';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/Operators';
+import { Users } from "../models/model.user";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/Operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthentificationService {
+  UsersUrl: string = "http://localhost:3000/userConnected";
 
-  UsersUrl:string = 'http://localhost:3000/userConnected';
-
-
-  constructor(private http: HttpClient ) { }
+  constructor(private http: HttpClient) {}
 
   getUsers(): Observable<Users[]> {
-    return this.http.get<Users[]>(this.UsersUrl)
-    .pipe(
-      tap(data => console.log(JSON.stringify(data)))
-    )
-
+    return this.http
+      .get<Users[]>(this.UsersUrl)
+      .pipe(tap(data => console.log(JSON.stringify(data))));
   }
-  addUser(){
-    
-  }
+  addUser() {}
 }
